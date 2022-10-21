b37caaf7eb9cd255c5f9224b1f35ff2f8905636d
Beatrice Hahn <hahnb@usc.edu>
2022-10-21 16:08:55 -0700

    Gatsby setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,18 +3,21 @@ module.exports = {
     title: `oit`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "LZ3CXfP8czz93PcghoHSMPWtJPJ6Y-N7Bz4aZlaiv54",
-      "spaceId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
+  plugins: [
+    "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-styled-components", {
     resolve: 'gatsby-source-filesystem',
     options: {
       "name": "images",
       "path": "./src/images/"
     },
     __key: "images"
-  }]
+  },
+  //{
+  //   resolve: 'gatsby-source-contentful',
+  //   options: {
+  //     "accessToken": "LZ3CXfP8czz93PcghoHSMPWtJPJ6Y-N7Bz4aZlaiv54",
+  //     "spaceId": "b7qunzg4xq2r"
+  //   }
+  // }
+]
 };
